c0d66996764632a37cbf03fcc0dc769406d492bc
miqitudou <miqitudou@gmail.com>
2024-03-26 12:26:58 +0800

    modified main.py

--- app/main.py
+++ app/main.py
@@ -1,31 +1,40 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
+import os
 
 app = FastAPI()
 
+
+UPLOAD_DIR = "file/rename"
+user_dir = os.path.expanduser("~")
+
 @app.post("/upload/image/")
 async def upload_image(file: UploadFile = File(...)):
-    # 保存上传的图片文件
+    
+    file_path = os.path.join(user_dir, UPLOAD_DIR, file.filename)
+
     contents = await file.read()
-    with open(file.filename, "wb") as f:
+    with open(file_path, "wb") as f:
         f.write(contents)
     
     return JSONResponse(content={"message": "Image uploaded successfully"}, status_code=200)
 
+
 @app.post("/upload/video/")
 async def upload_video(file: UploadFile = File(...)):
-    # 保存上传的视频文件
+    file_path = os.path.join(user_dir, UPLOAD_DIR, file.filename)
     contents = await file.read()
-    with open(file.filename, "wb") as f:
+    with open(file_path, "wb") as f:
         f.write(contents)
     
     return JSONResponse(content={"message": "Video uploaded successfully"}, status_code=200)
 
 @app.post("/upload/text/")
 async def upload_text(file: UploadFile = File(...)):
-    # 保存上传的文本文件
+    file_path = os.path.join(user_dir, UPLOAD_DIR, file.filename)
+    
     contents = await file.read()
-    with open(file.filename, "wb") as f:
+    with open(file_path, "wb") as f:
         f.write(contents)
     
     return JSONResponse(content={"message": "Text uploaded successfully"}, status_code=200)
